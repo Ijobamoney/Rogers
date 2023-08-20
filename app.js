6cfaae2acbfad13b287e8c32afb10b3a962accdc
Ijobamoney <142750542+Ijobamoney@users.noreply.github.com>
2023-08-20 14:36:27 -0600

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_5oqtts9", "template_xkkjosz", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
